2369ec09f70f730d0b3feaeb702bb26e9275e7ec
ainzatra <bkpg151@gmail.com>
2025-06-12 18:44:26 +0800

    feat:setup server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+app.use(require("./routes/index.js"));
+
+app.listen(3000, () => {
+  process.stdout.write("\x1Bc");
+  console.log("Server is running on http://localhost:3000");
+});
