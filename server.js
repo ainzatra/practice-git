2fcffce45593e58b2d9f8f8983f3ad07b7f64a25
ainzatra <bkpg151@gmail.com>
2025-06-14 13:35:30 +0800

    feat(server): add cors package

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
-
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(require("./routes/index.js"));
 
 app.listen(3000, () => {
