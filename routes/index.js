8b78554885b011d358f9756386e35d8beb9d4a19
ainzatra <bkpg151@gmail.com>
2025-06-14 12:44:08 +0800

    feat(product) : add controller, service, dao and routes

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 const userCtrl = require("../controller/user");
+const productCtrl = require("../controller/product");
 
 router.get("/user", userCtrl.getAllUsers);
 router.post("/user", userCtrl.createUser);
 router.put("/user/:id", userCtrl.updateUser);
+
+router.get("/product", productCtrl.getAll);
+router.get("/product/:id", productCtrl.getById);
+router.post("/product", productCtrl.create);
 
 module.exports = router;
