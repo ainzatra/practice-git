68e43e954b67ce005d7e578fcd103cac64d873c5
ainzatra <bkpg151@gmail.com>
2025-06-17 21:13:25 +0800

    feath(auth): add user auth middleware

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 const userCtrl = require("../controller/user");
+const userAuth = require("../middlewares/userAuth");
 
 router.get("/user", userCtrl.getAllUsers);
 router.post("/register", userCtrl.register);
 router.post("/login", userCtrl.login);
 router.put("/user/:id", userCtrl.updateUser);
+router.get("/protected", userAuth, async (req, res) => {
+  res.status(200).send("Hello there, this is a protected route.");
+});
 
 module.exports = router;
